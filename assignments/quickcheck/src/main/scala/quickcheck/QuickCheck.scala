4b6ede740c271354e460a6ff91ede48fcbf2db76
Sam Stephens <sam@growthintel.com>
2017-07-24 13:31:56 +0100

    Added tests

--- assignments/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ assignments/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -18,9 +18,38 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   )
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { (h: H) =>
+  property("findMin after insert min into heap gives min") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
+  }
+
+  property("findMind after two elements ins into empty heap is smallest") = forAll { (i: Int, j: Int) =>
+    val firstInserted = insert(i, empty)
+    val secondInserted = insert(j, firstInserted)
+
+    Math.min(i, j) == findMin(secondInserted)
+  }
+
+  property("Deleting minumum of 1 element heap is empty heap") = forAll { (i: Int) =>
+    val h = insert(i, empty)
+    isEmpty(deleteMin(h))
+  }
+
+  property("findMin/deleteMin/findMind etc. of arbitrary heap is ordered") = forAll { (h: H) =>
+    def isOrderedRecur(h: H, prevMin: Int): Boolean = {
+      if (isEmpty(h)) true
+      else if (findMin(h) < prevMin) false
+      else isOrderedRecur(deleteMin(h), findMin(h))
+    }
+    isOrderedRecur(h, Int.MinValue)
+  }
+
+  property("findMin of two melded arbitrary heaps is min of both heaps individually") = forAll { (h1: H, h2: H) =>
+    if (!isEmpty(h1) && !isEmpty(h2)) {
+      val (min1, min2) = (findMin(h1), findMin(h2))
+      Math.min(min1, min2) == findMin(meld(h1, h2))
+    }
+    else true
   }
 
 }
