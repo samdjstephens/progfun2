8e877cb25c70e0344fec5fdc7b2b99af37690476
Sam Stephens <sam@growthintel.com>
2017-08-07 13:32:14 +0100

    Finished assignment

--- assignments/calculator/src/main/scala/calculator/Calculator.scala
+++ assignments/calculator/src/main/scala/calculator/Calculator.scala
@@ -9,13 +9,20 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions map { case (name, exprSig) =>
+      name -> Var(eval(exprSig(), namedExpressions))
+    }
   }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+    case Literal(v) => v
+    case Ref(name) if references contains name => eval(references(name)(), references - name)
+    case Plus(a, b) => eval(a, references) + eval(b, references)
+    case Minus(a, b) => eval(a, references) - eval(b, references)
+    case Times(a, b) => eval(a, references) * eval(b, references)
+    case Divide(a, b) => eval(a, references) / eval(b, references)
+    case _ => Double.NaN
   }
 
   /** Get the Expr for a referenced variables.
