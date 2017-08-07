7ccced5ba31dced62bdc343bb5ab089f2556171c
Sam Stephens <sam@growthintel.com>
2017-08-07 13:04:22 +0100

    Added implementation of Polynomial

--- assignments/calculator/src/main/scala/calculator/Polynomial.scala
+++ assignments/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,18 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Var(b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Var({
+      val dlt = delta()
+      if (dlt < 0) Set.empty
+      else Set(
+        -b() + Math.sqrt(delta())/ (2 * a()),
+        -b() - Math.sqrt(delta())/ (2 * a())
+      )
+    })
   }
 }
